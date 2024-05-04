c614c560831cabda2df6fa290f87f86f13f89d3c
Cesar Espinosa <arionespinosam@gmail.com>
2024-05-03 18:33:10 -0600

    SECCION 6, Order Microservices, COMPLETE

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -8,7 +8,6 @@ import { envs, PRODUCT_SERVICE } from 'src/config';
   providers: [],
   imports: [
     ClientsModule.register([
-      
       { 
         name: PRODUCT_SERVICE, 
         transport: Transport.TCP,
@@ -17,7 +16,6 @@ import { envs, PRODUCT_SERVICE } from 'src/config';
           port: envs.productsMicroservicePort
         }
       },
-
     ]),
   ]
 })
